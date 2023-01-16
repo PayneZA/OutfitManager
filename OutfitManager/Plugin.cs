35d39bc1141449b169a845adaa98b4db3677a7e7
PayneZA <bekker.jonathan@gmail.com>
2023-01-16 18:59:58 +0200

    small fix

--- OutfitManager/Plugin.cs
+++ OutfitManager/Plugin.cs
@@ -213,7 +213,7 @@ namespace OutfitManager
                 int delay = 0;
                 if (!string.IsNullOrEmpty(outfit.GearSet))
                 {
-                /   this.Common.Functions.Chat.SendMessage("/gearset change " + outfit.GearSet.Trim());
+                  this.Common.Functions.Chat.SendMessage("/gearset change " + outfit.GearSet.Trim());
 
                     delay = 300;
                 }
